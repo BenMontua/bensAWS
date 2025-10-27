998cd1befea7f848833469a187c3ea6bf0381f0c
Benjamin-Montua02 <Benjamin.Montua02@vodafone.com>
2025-10-27 11:44:13 +0100

    fix: refined ec2 creation

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+# Connect 
+
+ssh -i ~/dev/bensAWS/github.pem ubuntu@18.192.26.79
--- bens_EC2.tf
+++ bens_EC2.tf
@@ -5,25 +5,25 @@
 variable "vpc_id" {
   description = "The VPC ID to launch the instance in."
   type        = string
-  default     = "vpc-xxxxxxxx"
+  default     = "vpc-015242e2c5efff6bf"
 }
 
 variable "subnet_id" {
   description = "The Subnet ID to launch the instance in."
   type        = string
-  default     = "subnet-xxxxxxxx"
+  default     = "subnet-0b054dbde2c35797c"
 }
 
 variable "ssh_ingress_cidr" {
   description = "The CIDR block allowed to SSH (e.g., 1.2.3.4/32)"
   type        = string
-  default     = "0.0.0.0/0"
+  default     = "109.90.180.219/32"
 }
 
 variable "instance_ami" {
   description = "AMI ID for the EC2 instance."
   type        = string
-  default     = "ami-xxxxxxxx"
+  default     = "ami-0735c860719883e1f"
 }
 
 variable "instance_type" {
@@ -54,23 +54,38 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "github" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name = "github"
+
+  user_data = <<-EOF
+    #!/bin/sh
+    # Update apk and install additional components
+    apt update
+    
+    apt install -y linux-headers-$(uname -r)
+
+    export version=580.95.05
+    export distro=ubuntu2404
+    export arch=arm64
+
+    wget https://developer.download.nvidia.com/compute/nvidia-driver/$version/local_installers/nvidia-driver-local-repo-$distro-$version_$arch.deb
+  EOF
 
   tags = {
-    Name = "example-ec2-instance"
+    Name = "github-ec2-instance"
   }
 }
 
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.github.id
 }
 
 output "public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.github.public_ip
 }
 
 
